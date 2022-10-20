0ea89c339ebc7a5d5a66c296e9a0b4e40004331e
tkl <tluk92@gmail.com>
2022-10-20 01:48:03 -0400

    wip: project page at mvp

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,8 @@
 // Imports
 //===
 
-import React from 'react'
-import styled from 'styled-components'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 //===
 // Component
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,6 +3,8 @@
 //===
 
 import styled from 'styled-components';
+import project1main from '../images/project1main.png';
+import project2main from '../images/project2main.png';
 import project3main from '../images/project3main.png';
 
 //==
@@ -32,22 +34,24 @@ const Projects = () => {
         <div className="main-container">
         <div className="section">
             <div className="container">
-                <h3>Project 1</h3>
+                <h3>Just for fun</h3>
                 <div className="card">
                     <div className="card-image">
-                        <img src="https://publish.purewow.net/wp-content/uploads/sites/2/2021/07/large-cat-breeds-cat.jpg?fit=728%2C524" alt="test" />
+                        <img src={project1main} alt="PokeNat Main" />
                     </div>
                     <div className="divider"></div>
                     <div className="card-content">
-                        <span className="card-title">Project1 SPAN</span>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim eligendi dolorem esse voluptates, accusantium, in ullam saepe repellat perspiciatis sequi incidunt numquam placeat eaque quo vel! Labore officiis suscipit facilis.</p>
+                        <span className="card-title">PokeNat</span>
+                        <p>
+                            A small personality quiz app that pulls images and information with an API.
+                        </p>
                     </div>
                     <div className="card-action">
                         <div className="row">
-                            <a href="http://www.google.com" target="_blank" rel="noreferrer" className="btn red darken-4">
+                            <a href="https://github.com/redsn/GA-SEI-Project-1" target="_blank" rel="noreferrer" className="btn red darken-4">
                                 Github
                             </a>
-                            <a href="http://www.google.com" target="_blank" rel="noreferrer" className="btn red darken-4">
+                            <a href="https://pokenat.netlify.app/" target="_blank" rel="noreferrer" className="btn red darken-4">
                                 Live Site
                             </a>
                         </div>
@@ -59,22 +63,24 @@ const Projects = () => {
 
         <div className="section">
             <div className="container">
-                <h3>Project 1</h3>
+                <h3>Full Stack Application</h3>
                 <div className="card">
                     <div className="card-image">
-                        <img src="https://publish.purewow.net/wp-content/uploads/sites/2/2021/07/large-cat-breeds-cat.jpg?fit=728%2C524" alt="test" />
+                        <img src={project2main} alt="BucketList Main" />
                     </div>
                     <div className="divider"></div>
                     <div className="card-content">
-                        <span className="card-title">Project1 SPAN</span>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim eligendi dolorem esse voluptates, accusantium, in ullam saepe repellat perspiciatis sequi incidunt numquam placeat eaque quo vel! Labore officiis suscipit facilis.</p>
+                        <span className="card-title">BucketList</span>
+                        <p>
+                            BucketList is a full-stack application for users who want to track media they've seen or plan to see. Users can also see their total watch time in the profile.
+                        </p>
                     </div>
                     <div className="card-action">
                         <div className="row">
-                            <a href="http://www.google.com" target="_blank" rel="noreferrer" className="btn red darken-4">
+                            <a href="https://github.com/redsn/Project3-Blist-Front" target="_blank" rel="noreferrer" className="btn red darken-4">
                                 Github
                             </a>
-                            <a href="http://www.google.com" target="_blank" rel="noreferrer" className="btn red darken-4">
+                            <a href="https://precious-capybara-c43215.netlify.app/" target="_blank" rel="noreferrer" className="btn red darken-4">
                                 Live Site
                             </a>
                         </div>
@@ -86,7 +92,7 @@ const Projects = () => {
 
         <div className="section">
             <div className="container">
-                <h3>Project 3: CRM</h3>
+                <h3>CRM</h3>
                 <div className="card">
                     <div className="card-image">
                         <img src={project3main} alt="WagTracker Landing Page" />
